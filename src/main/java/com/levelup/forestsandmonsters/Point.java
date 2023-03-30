4873010e02c326ead71ed817469d16e11476559a
Chiyo Chan <chiyochan9@gmail.com>
2023-03-30 20:13:31 +0000

    Added Point Class

---
+++ src/main/java/com/levelup/forestsandmonsters/Point.java
@@ -0,0 +1,12 @@
+package com.levelup.forestsandmonsters;
+
+public class Point {
+    int xCoordinate;
+    int yCoordinate;
+    public Point(int xCoordinate,int yCoordinate)
+    {
+        this.xCoordinate=xCoordinate;
+        this.yCoordinate=yCoordinate;
+    }
+    
+}
